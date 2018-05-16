81a11b42c13c83afa644688a7918d072e5f723d6
Abraham Hurtado <arhc.hc@gmail.com>
2018-05-15 17:55:43 -0700

    Creado modelo Album

---
+++ app/src/main/java/me/abraham/tapdiscos/Models/Album.kt
@@ -0,0 +1,14 @@
+package me.abraham.tapdiscos.Models
+
+import com.google.gson.annotations.SerializedName
+
+data class Album (
+        @SerializedName("id") val id : Int,
+        @SerializedName("artist") val artist : String,
+        @SerializedName("genre") val genre : String,
+        @SerializedName("title") val title: String,
+        @SerializedName("cover_url") val coverUrl : String
+//        @SerializedName("release") val release: Int
+) {
+
+}
